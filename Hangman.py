0e006019780e4dcc68574d8b3b711fd33ac1bd76
Kailash Turimella <kailash.turimella@gmail.com>
2020-06-09 20:59:05 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,10 @@
+"""
+Hangman game using python
+
+The computer chooses a random word from the list of words and displays the number of letters the word has
+  using blanks(underscores).
+The computer then fills in the blanks as you guess each letter.
+"""
 import random
 HANGMAN_PICS = ['''
   +---+
